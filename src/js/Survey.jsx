86dafefc766c125422eb3e280dd9131209dc3b09
Keith Donald <keith.donald@gmail.com>
2014-10-26 23:00:53 -0400

    HoH.jsx

--- src/js/Survey.jsx
+++ src/js/Survey.jsx
@@ -1,15 +1,26 @@
 /** @jsx React.DOM */
 var React = require('react');
-var Consent = require('./Consent.jsx');
+var ReactForms = require('react-forms');
+var Form = ReactForms.Form;
+
+var HoH = require('./HoH.jsx');
 
 module.exports = React.createClass({
   render: function() {
     return (
-      <form>
-        <Consent/>
-        <input type="button" value="Previous" />
-        <input type="submit" value="Next" />
+      <form onSubmit={this.onSubmit} className="Survey">
+        <Form ref="form" schema={HoH} component={React.DOM.div} />
+        <button type="submit">Submit</button>
       </form>
     )
+  },
+
+  onSubmit: function(e) {
+    e.preventDefault();
+    var form = this.refs.form;
+    if (form.getValidation().isFailure)  {
+      form.markDirty();
+    }
+    console.log(form.getValue());
   }
-});
+})
